016a0445f399722a2cf9207792a02c7917d03c8e
Facundototo <facutoto666@gmail.com>
2024-01-31 16:12:01 -0300

    excepciones del usuario y mejoras

--- _readme.md
+++ _readme.md
@@ -1,21 +1,30 @@
 ## Mini-Proyecto - Calculadora - 2024
 
-> **_Primer practica en JavaScript_**
-
 > [!NOTE]
 > _El codigo esta todo comentado de explicaciones (solo script.js)._
 
+> **_Primer practica en JavaScript_**
+
 #### --- Cosas que me faltan para la calculadora ---
 
-* Chequear que los operadores no se **repitan seguidamente** en la operacion.
-* Permitir **numeros negativos**. Ej => -10+1.
-* Que las cuentas despues de clickear el **'='** sigan si el usuario desea.
-* Permitir **numeros flotantes** en las operaciones.
-* Falta todo el diseño (por ahora lo menos importante).
+* [x] Chequear que los operadores no se **repitan seguidamente** en la operacion. 
+* [x] Permitir **numeros negativos**. Ej => -10+1.
+* [x] Que las cuentas despues de clickear el **'='** sigan si el usuario desea.
+* [x] Permitir **numeros flotantes** en las operaciones.
+* [ ] Falta todo el diseño (por ahora lo menos importante).
 
 ### --- Commits --- 
+
+> Del actual al antiguo
+
+| **'excepciones del usuario y mejoras'** - Fecha 30/1 |
+
+* Limite al usuario a que escriba bien las operaciones. Ej => 10+*10
+* Añadi numeros flotantes, con un fallo pero la cuenta da bien.
+* Que siga haciendo operaciones con el resultado de las operaciones.
 
 | **'empezando la calculadora'** - Fecha 30/1 |
 
 * Me quede pensando bastante la logica. Probando diferentes soluciones.
 * Lo que mas me costo fue como pensar y hacer el tema de las operaciones con mas de un operador.
+
--- script.js
+++ script.js
@@ -2,33 +2,48 @@
 let botones = document.querySelectorAll('button');      //Todos los botones del html almacenados en este array.
 let labelOperacion = document.getElementById('operacion');      //El label con la operacion.
 
-let valor = '',numeroStr = '';      //Declaracion e inicializacion de variables.
+let valor = '',numeroStr = '',botonAnterior = '';      //Declaracion e inicializacion de variables.
 let numeros = [],operadores = [];
+let primeroEsNeg = false, resEnviado = false;
 
 botones.forEach(boton => {      //Recorro cada boton con el .forEach() y les aplico a cada uno el evento 'click'.
     boton.addEventListener('click',() => {
 
-        labelOperacion.textContent += boton.textContent;        //Voy almacenando toda la operacion que se va a mostrar en pantalla.
-        valor = boton.textContent;      
+        if(validarIngreso(boton,botonAnterior)){        //Llamo al funcion validarIngreso() si me retorna true, quiere decir que no hay errores en la operacion.
+            botonAnterior = boton;      //Almaceno el botonAnterior con el fin de usarlo en validarIngreso().
+            labelOperacion.textContent += boton.textContent;        //Voy almacenando toda la operacion que se va a mostrar en pantalla.
+            valor = boton.textContent;      
 
-        if(boton.id === 'operador'){        //Si el boton es un operador.
-            operadores.push(valor);         //Pusheo el valor, al array operadores para despues hacer las cuentas.
-            numeros.push(parseFloat(numeroStr));        // Cuando el usuario toca un operador, almaceno los numeros (string) previos en un array. 
-            numeroStr = '';     
-        }else if(boton.id === 'numero'){        //Si el boton es un numero.
-            numeroStr += boton.textContent;     //Aca lo que hago es ir concatenando los numeros que va ingresando. Hasta que ponga un operador.
-        }else if (boton.id === 'igual'){        //Si el boton es el igual.
-            numeros.push(parseFloat(numeroStr));        //Pusheo el ultimo numero que quedo suelto. Sino en el primer if no lo pushea.
-            numeroStr = '';                             
-            calcularOperacion();        //Calculo el resultado de la operacion ingresada.                     
-            operadores = [];        //Reseteo el array operadores. 
-        }else if(boton.id === 'borrar'){        //Si el boton es el de borrar.
-            labelOperacion.textContent = '';       
-            operadores = [];        
-            numeros = [];       
+            if(boton.id === 'operador' && !primeroEsNeg){        //Si el boton es un operador y la !primeroEsNeg para que no me tome al - y lo pushee como un operador.
+                operadores.push(valor);         //Pusheo el valor, al array operadores para despues hacer las cuentas.
+                if(!resEnviado){numeros.push(parseFloat(numeroStr))};        // Cuando el usuario toca un operador, almaceno los numeros (string) previos en un array. 
+                numeroStr = '';                           //El if del renglon de arriba es porque si se toco '=' y toca un operador se pushea al numeroStr (que no tiene nada), cosa que no tendria que pasar.
+                resEnviado = false;     //Si estaba en true al pasar por aca lo pongo en false.
+            }else if(boton.id === 'numero' || boton.id === 'punto'){        //Si el boton es un numero o es un punto.
+                numeroStr += (primeroEsNeg)?'-'+boton.textContent:boton.textContent;     //Aca lo que hago es ir concatenando los numeros que va ingresando. Hasta que ponga un operador.
+                primeroEsNeg = false;       //Arriba si el primero es negativo le agrego al numero la negacion, desp defaulteo el boolean.
+            }else if (boton.id === 'igual'){        //Si el boton es el igual.
+                numeros.push(parseFloat(numeroStr));        //Pusheo el ultimo numero que quedo suelto. Sino en el primer if no lo pushea.
+                resEnviado = true;      //Aca se cambia a true el resEnviado ya que toco '='.
+                numeroStr = '';                             
+                calcularOperacion();        //Calculo el resultado de la operacion ingresada.                     
+                operadores = [];        //Reseteo el array operadores. 
+            }else if(boton.id === 'borrar'){        //Si el boton es el de borrar.
+                limpiarVariables();
+            }
         }
     });
 });
+
+const limpiarVariables = () => {        //Cree esta funcion mas que nada porque quedaba mal resetear todas las variables en el if.
+    labelOperacion.textContent = '';       
+    botonAnterior = '';
+    operadores = [];     
+    numeroStr = '';
+    valor = '';
+    resEnviado = false;   
+    numeros = [];     
+}
 
 const calcularOperacion = () => {       //Funcion que calcula la operacion.
     operadores.forEach((operador) => {      //Hago un .forEach() para recorrer los operadores almacenados.
@@ -50,3 +65,22 @@ const calcularOperacion = () => {       //Funcion que calcula la operacion.
     labelOperacion.textContent += numeros[0];       //Resultado final almacenado en numeros[0]. 
 }; 
 
+const validarIngreso = (boton,botonAnterior) => {       //Funcion retorna true si no encuentran fallas en la operacion cuando se escribe, false si es incorrecto el ingreso de datos.
+
+    let operadoresPuntoSeguidos = ((boton.id === 'operador' && botonAnterior.id === 'operador') || (boton.id === 'punto' && botonAnterior.id === 'punto'));
+    let clickIgualDespOperador =  (boton.id === 'igual' && botonAnterior.id === 'operador');
+    let igualesSeguidos = (boton.id === 'igual' && botonAnterior.id === 'igual');
+    let alPrincipioIgual = (labelOperacion.textContent.length === 0 && boton.id === 'igual');        //Masomenos los nombres de las variables explican las excepciones que pueden ocurrir.
+    let alPrincipioOperador = (labelOperacion.textContent.length === 0 && (boton.textContent !== '-' && boton.id === 'operador'));
+    let resultadoDespNumero = (resEnviado && (boton.id === 'numero' || boton.id === 'punto'));
+    let puntoDespOperadorIgual = ((boton.id === 'igual' && botonAnterior.id === 'punto') || (boton.id === 'operador' && botonAnterior.id === 'punto'));
+
+    if(operadoresPuntoSeguidos || clickIgualDespOperador || igualesSeguidos || alPrincipioIgual || alPrincipioOperador || resultadoDespNumero || puntoDespOperadorIgual){
+        return false;
+    }else if ((labelOperacion.textContent.length === 0 && boton.textContent === '-')){
+        primeroEsNeg = true;
+        return true;
+    }else {     //Este else para los numeros mas que nada.
+        return true;
+    }
+}
