9085007e642494023a9e23ad8a194d0b1ed64009
Facundototo <facutoto666@gmail.com>
2024-02-02 16:48:54 -0300

    resto, evento teclado y ajustes

--- _readme.md
+++ _readme.md
@@ -12,20 +12,30 @@
 * [x] Que las cuentas despues de clickear el **'='** sigan si el usuario desea.
 * [x] Permitir **numeros flotantes** en las operaciones.
 * [x] Hacer el diseño estetico de la pagina.
-* [ ] Que se puedan escribir las operaciones con el teclado.
-* [ ] Añadir el operador resto (%).
+* [x] Que se puedan escribir las operaciones con el teclado.
+* [x] Añadir el operador resto (%).
 ___
 
 #### --- Errores que siento que cometí en el proyecto ---
 
 * Al principio me costo buscar la forma de hacer las operaciones, pero siento que ahora con la solucion que le encontre no puedo por ejemplo hacer los () de la calculadora. Los parentesis lo que tiene es que se hacen primero las operaciones que estan entre () y despues las otras.
 
-* Al no acordarme conceptos y buenas practicas del HTML, recien di cuenta de que los id son unicos y las clases no. Y bueno, agrupe a los botones con ids (numero,operador). Lo pongo aca como error pero creo que puedo solucionarlo facilmente.
+*  **SOLUCIONADO** - Al no acordarme conceptos y buenas practicas del HTML, recien di cuenta de que los id son unicos y las clases no. Y bueno, agrupe a los botones con ids (numero,operador). Lo pongo aca como error pero creo que puedo solucionarlo facilmente.
+
+* Tengo un error que no se me ocurre como solucionarlo. El boton '.' se puede poner varias veces en un numero, ej => 10 + 1.5.7.5... Lo bueno de esto es que el resultado no me da mal, solo toma hasta el primer punto es decir => 10 + 1.5, asi que lo voy a dejar asi. Pero bueno no encontre una forma de solucionarlo.
+
 ___
 
 ### --- Commits --- 
 
 > Del actual al antiguo
+
+| **'resto, evento teclado y ajustes'** - Fecha 2/02 |
+
+* Añadi la operacion resto, ya es funcional.
+* Ahora el usuario puede escribir las operaciones con el teclado.
+* Solucione uno de los puntos del apartado de errores, era facil.
+* Añadi otro error que me acorde de antes.
 
 | **'diseño,errores y cosas que faltan'** - Fecha 1/02 |
 
--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 
-let botones = document.querySelectorAll('button');      //Todos los botones del html almacenados en este array.
+let botones = [...document.querySelectorAll('button')];      //Todos los botones del html almacenados en este array.
 let labelOperacion = document.getElementById('operacion');      //El label con la operacion.
 
 let valor = '',numeroStr = '',botonAnterior = '';      //Declaracion e inicializacion de variables.
 let numeros = [],operadores = [];
 let primeroEsNeg = false, resEnviado = false;
+let res = 0;
 
-//Falta que cuando pase el igual las operaciones anteriores desaparezcan si sigue escribiendo.
-//Y hacer la operacion de resto.
-// Ver si puedo disenar la scrollbar.
-
+window.addEventListener('keydown', event =>{        //Listener por si el usuario quiere teclear la operacion.
+    const keyPresionada = botones.find(boton => boton.textContent === event.key);       //Retorno mediante .find() el boton que coincida con la keycode que toco el usuario.
+    if(keyPresionada !== undefined){ manipularEventoBotones(keyPresionada); }       // Esta condicion porque el .find() cuando no encuentra retorna undefined.Si se encontro llamo a la funcion.
+})
 botones.forEach(boton => {      //Recorro cada boton con el .forEach() y les aplico a cada uno el evento 'click'.
     boton.addEventListener('click',() => {
-
-        if(validarIngreso(boton,botonAnterior)){        //Llamo al funcion validarIngreso() si me retorna true, quiere decir que no hay errores en la operacion.
-            botonAnterior = boton;      //Almaceno el botonAnterior con el fin de usarlo en validarIngreso().
-            labelOperacion.textContent += boton.textContent;        //Voy almacenando toda la operacion que se va a mostrar en pantalla.
-            valor = boton.textContent;      
-
-            if(boton.id === 'operador' && !primeroEsNeg){        //Si el boton es un operador y la !primeroEsNeg para que no me tome al - y lo pushee como un operador.
-                operadores.push(valor);         //Pusheo el valor, al array operadores para despues hacer las cuentas.
-                if(!resEnviado){numeros.push(parseFloat(numeroStr))};        // Cuando el usuario toca un operador, almaceno los numeros (string) previos en un array. 
-                numeroStr = '';                           //El if del renglon de arriba es porque si se toco '=' y toca un operador se pushea al numeroStr (que no tiene nada), cosa que no tendria que pasar.
-                resEnviado = false;     //Si estaba en true al pasar por aca lo pongo en false.
-            }else if(boton.id === 'numero' || boton.id === 'punto'){        //Si el boton es un numero o es un punto.
-                numeroStr += (primeroEsNeg)?'-'+boton.textContent:boton.textContent;     //Aca lo que hago es ir concatenando los numeros que va ingresando. Hasta que ponga un operador.
-                primeroEsNeg = false;       //Arriba si el primero es negativo le agrego al numero la negacion, desp defaulteo el boolean.
-            }else if (boton.id === 'igual'){        //Si el boton es el igual.
-                numeros.push(parseFloat(numeroStr));        //Pusheo el ultimo numero que quedo suelto. Sino en el primer if no lo pushea.
-                resEnviado = true;      //Aca se cambia a true el resEnviado ya que toco '='.
-                numeroStr = '';                             
-                calcularOperacion();        //Calculo el resultado de la operacion ingresada.                     
-                operadores = [];        //Reseteo el array operadores. 
-            }else if(boton.id === 'borrar'){        //Si el boton es el de borrar.
-                limpiarVariables();
-            }
-        }
+        manipularEventoBotones(boton);
     });
 });
 
+
+const manipularEventoBotones = (boton) => {     //Meti el codigo en esta funcion para reutilizarlo, en el caso de que el usuario escriba con el teclado.
+
+    if(validarIngreso(boton,botonAnterior)){        //Llamo al funcion validarIngreso() si me retorna true, quiere decir que no hay errores en la operacion.
+
+        almacenarDatos(boton);      //Guardo valores para despues.
+
+        if(boton.className === 'operador' && !primeroEsNeg){        //Si el boton es un operador y la !primeroEsNeg para que no me tome al - y lo pushee como un operador.
+            operadores.push(valor);         //Pusheo el valor, al array operadores para despues hacer las cuentas.
+            if(!resEnviado){numeros.push(parseFloat(numeroStr))}        // Cuando el usuario toca un operador, almaceno los numeros (string) previos en un array. 
+            else{labelOperacion.textContent = (res + boton.textContent);}       //Si toca '=' y paso por este if, esta escribiendo otra operacion con el resultado,por ende se borra lo anterior.
+            numeroStr = '';                           //El if del renglon de arriba es porque si se toco '=' y toca un operador se pushea al numeroStr (que no tiene nada), cosa que no tendria que pasar.
+            resEnviado = false;     //Si estaba en true al pasar por aca lo pongo en false.
+        }else if(boton.className === 'numero' || boton.className === 'punto'){        //Si el boton es un numero o es un punto.
+            numeroStr += (primeroEsNeg)?'-'+boton.textContent:boton.textContent;     //Aca lo que hago es ir concatenando los numeros que va ingresando. Hasta que ponga un operador.
+            primeroEsNeg = false;       //Arriba si el primero es negativo le agrego al numero la negacion, desp defaulteo el boolean.
+        }else if (boton.className === 'igual'){        //Si el boton es el igual.
+            numeros.push(parseFloat(numeroStr));        //Pusheo el ultimo numero que quedo suelto. Sino en el primer if no lo pushea.
+            resEnviado = true;      //Aca se cambia a true el resEnviado ya que toco '='.
+            numeroStr = '';                             
+            calcularOperacion();        //Calculo el resultado de la operacion ingresada.      
+            operadores = [];        //Reseteo el array operadores. 
+        }else if(boton.className === 'borrar'){        //Si el boton es el de borrar.
+            limpiarVariables();
+        }
+    }
+}
+
+
 const limpiarVariables = () => {        //Cree esta funcion mas que nada porque quedaba mal resetear todas las variables en el if.
-    labelOperacion.textContent = '';       
+    labelOperacion.textContent = '';     
     botonAnterior = '';
     operadores = [];     
     numeroStr = '';
     valor = '';
     resEnviado = false;   
+    res = 0;
     numeros = [];     
 }
 
@@ -64,20 +72,24 @@ const calcularOperacion = () => {       //Funcion que calcula la operacion.
             case '/':
                 numeros.splice(0,2,numeros[0]/numeros[1]);
                 break;
+            case '%':
+                numeros.splice(0,2,numeros[0]%numeros[1]);
+                break;
         }
     });
-    labelOperacion.textContent += numeros[0];       //Resultado final almacenado en numeros[0]. 
+    res = numeros[0];
+    labelOperacion.textContent += res;       //Resultado final almacenado en numeros[0]. 
 }; 
 
 const validarIngreso = (boton,botonAnterior) => {       //Funcion retorna true si no encuentran fallas en la operacion cuando se escribe, false si es incorrecto el ingreso de datos.
 
-    let operadoresPuntoSeguidos = ((boton.id === 'operador' && botonAnterior.id === 'operador') || (boton.id === 'punto' && botonAnterior.id === 'punto'));
-    let clickIgualDespOperador =  (boton.id === 'igual' && botonAnterior.id === 'operador');
-    let igualesSeguidos = (boton.id === 'igual' && botonAnterior.id === 'igual');
-    let alPrincipioIgual = (labelOperacion.textContent.length === 0 && boton.id === 'igual');        //Masomenos los nombres de las variables explican las excepciones que pueden ocurrir.
-    let alPrincipioOperador = (labelOperacion.textContent.length === 0 && (boton.textContent !== '-' && boton.id === 'operador'));
-    let resultadoDespNumero = (resEnviado && (boton.id === 'numero' || boton.id === 'punto'));
-    let puntoDespOperadorIgual = ((boton.id === 'igual' && botonAnterior.id === 'punto') || (boton.id === 'operador' && botonAnterior.id === 'punto'));
+    let operadoresPuntoSeguidos = ((boton.className === 'operador' && botonAnterior.className === 'operador') || (boton.className === 'punto' && botonAnterior.className === 'punto'));
+    let clickIgualDespOperador =  (boton.className === 'igual' && botonAnterior.className === 'operador');
+    let igualesSeguidos = (boton.className === 'igual' && botonAnterior.className === 'igual');
+    let alPrincipioIgual = (labelOperacion.textContent.length === 0 && boton.className === 'igual');        //Masomenos los nombres de las variables explican las excepciones que pueden ocurrir.
+    let alPrincipioOperador = (labelOperacion.textContent.length === 0 && (boton.textContent !== '-' && boton.className === 'operador'));
+    let resultadoDespNumero = (resEnviado && (boton.className === 'numero' || boton.className === 'punto'));
+    let puntoDespOperadorIgual = ((boton.className === 'igual' && botonAnterior.className === 'punto') || (boton.className === 'operador' && botonAnterior.className === 'punto'));
 
     if(operadoresPuntoSeguidos || clickIgualDespOperador || igualesSeguidos || alPrincipioIgual || alPrincipioOperador || resultadoDespNumero || puntoDespOperadorIgual){
         return false;
@@ -87,4 +99,10 @@ const validarIngreso = (boton,botonAnterior) => {       //Funcion retorna true s
     }else {     //Este else para los numeros mas que nada.
         return true;
     }
-}
+};
+
+const almacenarDatos = (boton) => {
+    botonAnterior = boton;      //Almaceno el botonAnterior con el fin de usarlo en validarIngreso().
+    labelOperacion.textContent += boton.textContent;     //Voy almacenando toda la operacion que se va a mostrar en pantalla.
+    valor = boton.textContent;      
+};
